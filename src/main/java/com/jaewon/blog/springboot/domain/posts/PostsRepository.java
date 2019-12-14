c12512feff3ce6ed210bf0f4defa7a9ff4edec07
jaewon0913 <jaewon9937@gmail.com>
2019-12-14 17:12:16 +0900

    CRUD VIEW 생성[머스테치]

--- src/main/java/com/jaewon/blog/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jaewon/blog/springboot/domain/posts/PostsRepository.java
@@ -1,9 +1,15 @@
 package com.jaewon.blog.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     //  DB Layer 접근자 -> JPA에선 Repository라고 부르며 인터페이스로 생성한다.
     //  자동으로 CRUD 메소드가 생성된다.
     //  @Repository를 추가할 필요가 없다.(Entity 클래스와 기본 Entity Repository는 함께 위치해야 한다.)
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jaewon/blog/springboot/service/posts/PostsService.java
+++ src/main/java/com/jaewon/blog/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jaewon.blog.springboot.service.posts;
 
 import com.jaewon.blog.springboot.domain.posts.Posts;
 import com.jaewon.blog.springboot.domain.posts.PostsRepository;
+import com.jaewon.blog.springboot.web.dto.PostsListResponseDto;
 import com.jaewon.blog.springboot.web.dto.PostsResponseDto;
 import com.jaewon.blog.springboot.web.dto.PostsSaveRequestDto;
 import com.jaewon.blog.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,20 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) //  map(PostListResponseDto::new) == map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id){
+        Posts posts =postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
