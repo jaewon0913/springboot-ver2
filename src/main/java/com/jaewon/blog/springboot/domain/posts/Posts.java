4cba2cdd8285621cd294c4540bb87732145d5594
jaewon0913 <jaewon9937@gmail.com>
2019-12-14 16:18:58 +0900

    CRUD JPA로 다루기

--- src/main/java/com/jaewon/blog/springboot/domain/posts/Posts.java
+++ src/main/java/com/jaewon/blog/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jaewon.blog.springboot.domain.posts;
 
+import com.jaewon.blog.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter //  Getter 메소드 생성
 @NoArgsConstructor  //  기본 생성자 자동 추가.(ex: public Post(){}와 같은 효과)
 @Entity //  테이블과 링크될 클래스임을 나타낸다. 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍(_)으로 테이블 이름을 매칭할 수 있다.(ex: JaewonBoard.java -> jaewon_board table)
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id //  해당 테이블의 PK(Primary Key)필드를 나타낸다.
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,9 +28,14 @@ public class Posts {
     private String author;
 
     @Builder    // 해당 클래스의 빌더 패턴 클래스를 생성, 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
-    public Posts (String title, String content, String author){
+    public Posts(String title, String content, String author) {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
---
+++ src/main/java/com/jaewon/blog/springboot/service/posts/PostsService.java
@@ -0,0 +1,38 @@
+package com.jaewon.blog.springboot.service.posts;
+
+import com.jaewon.blog.springboot.domain.posts.Posts;
+import com.jaewon.blog.springboot.domain.posts.PostsRepository;
+import com.jaewon.blog.springboot.web.dto.PostsResponseDto;
+import com.jaewon.blog.springboot.web.dto.PostsSaveRequestDto;
+import com.jaewon.blog.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
